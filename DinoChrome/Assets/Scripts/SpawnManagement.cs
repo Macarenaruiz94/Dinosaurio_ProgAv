3510efae3e16ae2190a93b5e76a90b63de7f786b
Macarena Ruiz <contactomruiz@yahoo.com>
2022-08-27 12:12:37 -0300

    spawn time ajuste

--- DinoChrome/Assets/Scripts/SpawnManagement.cs
+++ DinoChrome/Assets/Scripts/SpawnManagement.cs
@@ -9,11 +9,14 @@ public class SpawnManagement : MonoBehaviour
     [SerializeField] private GameObject EnemyUp;
     [SerializeField] private GameObject SpotDown;
     [SerializeField] private GameObject SpotUp;
-    [SerializeField] private float TimeSpawn;
-    [SerializeField] private float SpawnRate;
+    [SerializeField] private float TimeSpawnDown;
+    [SerializeField] private float SpawnRateDown;
+    [SerializeField] private float TimeSpawnUp;
+    [SerializeField] private float SpawnRateUp;
     void Start()
     {
-        InvokeRepeating("SpawnEnemies", TimeSpawn, SpawnRate);
+        InvokeRepeating("SpawnEnemiesDown", TimeSpawnDown, SpawnRateDown);
+        InvokeRepeating("SpawnEnemiesUp", TimeSpawnUp, SpawnRateUp);
     }
 
     void Update()
@@ -21,15 +24,19 @@ public class SpawnManagement : MonoBehaviour
         
     }
 
-    private void SpawnEnemies()
+    private void SpawnEnemiesDown()
     {
         Vector3 spawnPos = new Vector3(0, 0, 0);
         spawnPos = new Vector3(SpotDown.transform.position.x, SpotDown.transform.position.y, 0);
 
+        GameObject downEnemy = Instantiate(EnemyDown, spawnPos, gameObject.transform.rotation);
+    }
+
+    private void SpawnEnemiesUp()
+    {
         Vector3 spawnPosTwo = new Vector3(0, 0, 0);
         spawnPosTwo = new Vector3(SpotUp.transform.position.x, SpotUp.transform.position.y, 0);
 
-        GameObject downEnemy = Instantiate(EnemyDown, spawnPos, gameObject.transform.rotation);
         GameObject upEnemy = Instantiate(EnemyUp, spawnPosTwo, gameObject.transform.rotation);
     }
 }
