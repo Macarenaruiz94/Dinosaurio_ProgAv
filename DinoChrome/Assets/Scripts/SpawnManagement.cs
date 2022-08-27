deb5d15ef65e3ec7f98cc91a8cb7892610a2d95f
Macarena Ruiz <contactomruiz@yahoo.com>
2022-08-27 12:03:35 -0300

    spawn ajustes

--- DinoChrome/Assets/Scripts/SpawnManagement.cs
+++ DinoChrome/Assets/Scripts/SpawnManagement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,27 @@ public class SpawnManagement : MonoBehaviour
     [SerializeField] private GameObject EnemyUp;
     [SerializeField] private GameObject SpotDown;
     [SerializeField] private GameObject SpotUp;
+    [SerializeField] private float TimeSpawn;
+    [SerializeField] private float SpawnRate;
     void Start()
     {
-        
+        InvokeRepeating("SpawnEnemies", TimeSpawn, SpawnRate);
     }
 
     void Update()
     {
         
+    }
+
+    private void SpawnEnemies()
+    {
+        Vector3 spawnPos = new Vector3(0, 0, 0);
+        spawnPos = new Vector3(SpotDown.transform.position.x, SpotDown.transform.position.y, 0);
+
+        Vector3 spawnPosTwo = new Vector3(0, 0, 0);
+        spawnPosTwo = new Vector3(SpotUp.transform.position.x, SpotUp.transform.position.y, 0);
+
+        GameObject downEnemy = Instantiate(EnemyDown, spawnPos, gameObject.transform.rotation);
+        GameObject upEnemy = Instantiate(EnemyUp, spawnPosTwo, gameObject.transform.rotation);
     }
 }
